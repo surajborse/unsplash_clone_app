a5b694a94282bf2a1572579abcadcfc2d9aa8475
surajborse <83261596+surajborse@users.noreply.github.com>
2021-08-18 13:12:33 +0530

    Update Nav.js

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -22,10 +22,10 @@ const Nav = () => {
 
             <ul class="navbar-nav m-auto mb-2 mb-lg-0">
               <li className="nav-item ">
-                <a className="nav-link active" aria-current="page" href="#home">Home</a>
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li className="nav-item ">
-                <a className="nav-link active" href="#home">Barnd</a>
+                <a className="nav-link active" href="#home">Brand</a>
               </li>
               <li className="nav-item ">
                 <a className="nav-link active" href="#home">Export</a>
